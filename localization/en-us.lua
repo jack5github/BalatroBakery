72e7fa19e20afb51c3324dace7978597d22b8bd1
Jack5 <jack5answers@gmail.com>
2025-07-01 12:51:17 +1000

    Undo accidental de-capitalisation

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -379,7 +379,7 @@ return {
         Other = {
             Bakery_charm = {
                 name = "Charm",
-                text = { "Only one charm may", "be equipped at a time,", "purchasing a new charm", "replaces an old one" }
+                text = { "Only one Charm may", "be equipped at a time,", "purchasing a new Charm", "replaces an old one" }
             },
             undiscovered_bakerycharm = {
                 name = "Undiscovered",
